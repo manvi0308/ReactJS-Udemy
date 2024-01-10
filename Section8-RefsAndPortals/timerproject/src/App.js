c4b99df0b66f8c36b967122336fdf19a205830f4
MChaddha <147035815+MChaddha@users.noreply.github.com>
2024-01-10 14:42:45 +0530

    Added timer challenges

--- Section8-RefsAndPortals/timerproject/src/App.js
+++ Section8-RefsAndPortals/timerproject/src/App.js
@@ -1,10 +1,17 @@
 import Player from './components/Player.js';
+import TimerChallenge from './components/TimerChallenge.js';
 
 function App() {
   return (
     <>
-      <Player userName={'Manvi'}/>
-      <div id="challenges"></div>
+      <Player/>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}/>
+        <TimerChallenge title="Not Easy" targetTime={5}/>
+        <TimerChallenge title="Getting Tough" targetTime={10}/>
+        <TimerChallenge title="Pros Only" targetTime={20}/>
+
+      </div>
     </>
   );
 }
